5e0abf185965fbacbfcd56f5aa236345632edcd7
Thiago Tolotti <thiagotolotti@gmail.com>
2025-01-19 14:26:20 -0300

    adds education to info page, rearranges sections inside info page

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -51,102 +51,140 @@ const experience: {
   },
 ];
 
+const education: {
+  name: string;
+  institution: string;
+  date: string;
+  logo: string;
+}[] = [
+  {
+    name: 'Postgraduate Degree in Agile Software Development',
+    institution: 'Federal University of Paraná (UFPR)',
+    date: '2025 → Present',
+    logo: '/ufpr.jpg',
+  },
+  {
+    name: 'Major in Design',
+    institution: 'Federal University of Technology (UTFPR)',
+    date: '2018 → 2023',
+    logo: '/utfpr.png',
+  },
+  {
+    name: 'Technical Qualification in Mechanics',
+    institution: 'Federal University of Technology (UTFPR)',
+    date: '2014 → 2018',
+    logo: '/utfpr.png',
+  },
+];
+
 export default async function InfoPage() {
   return (
-    <main className="px-4 md:px-0">
+    <main className="px-4 md:px-0 flex flex-col gap-4">
       <PageHeader title="Information" />
-      {/*<section className="pb-8 prose prose-lg">
-      {/*<section className="pb-8 prose prose-lg">
-        <p>In progress....</p>
-         <p>
-          Hi there, I&apos;m Alex. I build my own products, along side working
-          with some amazing companies to help them build theirs.
-        </p>
-        <p>
-          Most recently, I was working with MoonPay to help build the future of
-          payments. Before that, I was working with Monzo - the fastest growing
-          bank in the UK. Originally joining as a product designer, I
-          transitioned into a web engineer role where I worked with a variety of
-          teams to help rethink modern day banking for their millions of
-          customers.
-        </p>
 
-        <p>
-          I also worked with Kalo (which eventually became Polywork) where I was
-          the first UI Engineering hire. My responsibilities included building
-          out the design system as well as ensuring a high bar of design quality
-          across the product.
-        </p>
+      <Skills />
 
-        <p>
-          I&apos;ve always bounced somewhere between design and engineering,
-          however the common thread has always been my love for building things
-          that people love to use.
-        </p>
+      <Experiences />
 
-        <p>
-          If you&apos;re interested in working together, feel free to reach out
-          to me here:{' '}
-          <Link href="mailto:hey@alexpate.com">hey@alexpate.com</Link>.
-        </p>
-      </section> */}
-
-      <section>
-        <Title as="h2" variant="secondary" className="mb-4 mt-8 ">
-          Skills & Technologies
-        </Title>
-
-        <p className="text-base">
-          Next.js · React · Redux · Tailwind · Typescript · Javascript · Jest ·
-          CSS · HTML · Git · Vite · Node.js · Express · Docker · Design Patterns
-          · Responsive UI · UX Research · Figma · Illustrator · Photoshop ·
-          Agile · Design Thinking · User testing · User interviews
-        </p>
-      </section>
-
-      <section>
-        <Title as="h2" variant="secondary" className="mb-4 mt-8 ">
-          Experience
-        </Title>
-
-        <div className="divide-y divide-slate-200">
-          {experience.map((exp) => {
-            return (
-              <div className="flex gap-4 py-6" key={exp.date}>
-                <Image
-                  width={56}
-                  height={56}
-                  src={exp.logo}
-                  alt={`${exp.company} logo`}
-                  className="w-14 h-14 rounded-xl"
-                />
-                <div className="flex flex-col col-span-9">
-                  <span className="text-slate-800 text-xl font-semibold">
-                    {exp.company}
-                  </span>
-                  <span className="text-slate-700 text-lg">{exp.role}</span>
-                  <span className="block mt-4 text-slate-500 col-span-2 text-sm font-medium tracking-tighter font-mono">
-                    {exp.date}
-                  </span>
-                  <span className="mt-4 text-slate-800 flex flex-col gap-4">
-                    {exp.description?.map((paragraph, index) => (
-                      <p key={index}>{paragraph}</p>
-                    ))}
-                  </span>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-
-        <Link
-          href="/resume.pdf"
-          target="_blank"
-          className="block my-8 text-slate-500 text-sm font-medium"
-        >
-          Download resume →
-        </Link>
-      </section>
+      <Education />
     </main>
+  );
+}
+
+function Skills() {
+  return (
+    <section>
+      <Title as="h2" variant="secondary" className="mb-4 mt-8 ">
+        Skills & Technologies
+      </Title>
+
+      <p className="text-base">
+        Next.js · React · Redux · Tailwind · Typescript · Javascript · Jest ·
+        CSS · HTML · Git · Vite · Node.js · Express · Docker · Design Patterns ·
+        Responsive UI · UX Research · Figma · Illustrator · Photoshop · Agile ·
+        Design Thinking · User testing · User interviews
+      </p>
+    </section>
+  );
+}
+
+function Experiences() {
+  return (
+    <section>
+      <Title as="h2" variant="secondary" className="mb-4 mt-8 ">
+        Experience
+      </Title>
+
+      <div className="divide-y divide-slate-200">
+        {experience.map((exp) => {
+          return (
+            <div className="flex gap-4 py-6" key={exp.date}>
+              <Image
+                width={56}
+                height={56}
+                src={exp.logo}
+                alt={`${exp.company} logo`}
+                className="w-14 h-14 rounded-xl"
+              />
+              <div className="flex flex-col col-span-9">
+                <span className="text-slate-800 text-xl font-semibold">
+                  {exp.company}
+                </span>
+                <span className="text-slate-700 text-lg">{exp.role}</span>
+                <span className="block mt-4 text-slate-500 col-span-2 text-sm font-medium tracking-tighter font-mono">
+                  {exp.date}
+                </span>
+                <span className="mt-4 text-slate-800 flex flex-col gap-4">
+                  {exp.description.map((paragraph, index) => (
+                    <p key={index}>{paragraph}</p>
+                  ))}
+                </span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <Link
+        href="/resume.pdf"
+        target="_blank"
+        className="block my-8 text-slate-500 text-sm font-medium"
+      >
+        Download resume →
+      </Link>
+    </section>
+  );
+}
+
+function Education() {
+  return (
+    <section>
+      <Title as="h2" variant="secondary" className="mb-4 mt-8 ">
+        Education
+      </Title>
+
+      {education.map((exp) => {
+        return (
+          <div className="flex gap-4 py-6" key={exp.date}>
+            <Image
+              width={56}
+              height={56}
+              src={exp.logo}
+              alt={`${exp.institution} logo`}
+              className="w-14 h-14 rounded-xl"
+            />
+            <div className="flex flex-col col-span-9">
+              <span className="text-slate-800 text-xl font-semibold">
+                {exp.name}
+              </span>
+              <span className="text-slate-700 text-lg">{exp.institution}</span>
+              <span className="block mt-4 text-slate-500 col-span-2 text-sm font-medium tracking-tighter font-mono">
+                {exp.date}
+              </span>
+            </div>
+          </div>
+        );
+      })}
+    </section>
   );
 }
